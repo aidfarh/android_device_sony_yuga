12013f3bb412e201f6145907f6459abab2ffd712
CamberCreak <cambercreak@gmail.com>
2014-09-04 08:58:50 +0100

    setup for OmniRom

--- omni_yuga.mk
+++ omni_yuga.mk
@@ -1,14 +1,15 @@
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
-# Inherit CM common GSM stuff.
-#$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
-$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+# Inherit GSM configuration
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit Omni Product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
 
-PRODUCT_NAME := carbon_yuga
+PRODUCT_NAME := omni_yuga
+PRODUCT_DEVICE := yuga
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo carbon_yuga-eng
+add_lunch_combo omni_yuga-userdebug
